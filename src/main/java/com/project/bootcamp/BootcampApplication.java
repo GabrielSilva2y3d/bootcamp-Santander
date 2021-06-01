e4a28f7c34ceed4eb4a43be0809e327949ea1ce0
GabrielSilva2y3d <74156876+GabrielSilva2y3d@users.noreply.github.com>
2021-06-01 17:57:29 -0300

    Só botando um titulo

--- src/main/java/com/project/bootcamp/BootcampApplication.java
+++ src/main/java/com/project/bootcamp/BootcampApplication.java
@@ -18,7 +18,7 @@ public class BootcampApplication {
 	@Bean
 	public OpenAPI customOpenAPI(){
 		return new OpenAPI().info(new Info()
-			.title("")
+			.title("Bootcamp Dev Week DIO - Santander")
 			.version("1.0")
 			.termsOfService("http://swagger.io/terms")
 			.license(new License().name("Apache 2.0")
